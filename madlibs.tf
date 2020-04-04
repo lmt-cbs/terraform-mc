9f880f5547afb1ffc00d4271491b7cfd16774388
lmtbelmontem <lmtbelmonte@gmail.com>
2020-04-04 19:58:05 +0200

    Pruebas con el resource local_file y templates

--- madlibs.tf
+++ madlibs.tf
@@ -53,3 +53,17 @@ resource "random_shuffle" "random_numbers" {
   count = var.num_files
   input = var.words["numbers"]
 }
+
+resource "local_file" "mad_lib" {
+  count = var.num_files
+  content = templatefile("alice.txt",
+    {
+      nouns=random_shuffle.random_nouns[count.index].result
+      adjectives=random_shuffle.random_adjectives[count.index].result
+      verbs=random_shuffle.random_verbs[count.index].result
+      adverbs=random_shuffle.random_adverbs[count.index].result
+      numbers=random_shuffle.random_numbers[count.index].result
+    }
+  )
+   filename = "madlibs/madlib-${count.index}.txt"
+}
