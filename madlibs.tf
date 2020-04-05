bddcc4fca5962d2c7d8e4978844b208fc9928b02
lmtbelmontem <lmtbelmonte@gmail.com>
2020-04-05 06:37:05 +0200

    Modificacion recurso local_file

--- madlibs.tf
+++ madlibs.tf
@@ -58,12 +58,11 @@ resource "local_file" "mad_lib" {
   count = var.num_files
   content = templatefile("alice.txt",
     {
-      nouns=random_shuffle.random_nouns[count.index].result
-      adjectives=random_shuffle.random_adjectives[count.index].result
-      verbs=random_shuffle.random_verbs[count.index].result
-      adverbs=random_shuffle.random_adverbs[count.index].result
-      numbers=random_shuffle.random_numbers[count.index].result
-    }
-  )
-   filename = "madlibs/madlib-${count.index}.txt"
+        nouns=random_shuffle.random_nouns[count.index].result
+        adjectives=random_shuffle.random_adjectives[count.index].result
+        verbs=random_shuffle.random_verbs[count.index].result
+        adverbs=random_shuffle.random_adverbs[count.index].result
+        numbers=random_shuffle.random_numbers[count.index].result
+    })
+    filename = "madlibs/madlib-${count.index}.txt"
 }
